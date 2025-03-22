fd5554955addc2263591d13ee194d0422cf51665
Rumeysataskapan <rumeysajavadeveloper@gmail.com>
2025-03-22 13:08:22 +0300

    Remove application.properties from repository

--- src/main/java/com/Cinetime/Repository/MovieRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Cinetime.Repository;
-
-import com.Cinetime.entity.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-    // You can add custom queries here.
-}
