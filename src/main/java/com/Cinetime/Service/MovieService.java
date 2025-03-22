1a0434e6e7637b939e539e8f050a7ba04b3deca4
Rumeysataskapan <rumeysajavadeveloper@gmail.com>
2025-03-22 20:59:37 +0300

    New Code

--- src/main/java/com/Cinetime/Service/MovieService.java
+++ src/main/java/com/Cinetime/Service/MovieService.java
@@ -1,52 +1,179 @@
-package com.Cinetime.Service;
+package com.Cinetime.service;
 
-import com.Cinetime.Repository.MovieRepository;
+
 import com.Cinetime.entity.Movie;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.Cinetime.entity.PosterImage;
+import com.Cinetime.enums.MovieStatus;
+import com.Cinetime.helpers.MovieHelper;
+import com.Cinetime.helpers.PageableHelper;
+import com.Cinetime.payload.dto.MovieRequest;
+import com.Cinetime.payload.mappers.MovieMapper;
+import com.Cinetime.payload.messages.SuccessMessages;
+import com.Cinetime.payload.response.MovieResponse;
+import com.Cinetime.payload.response.ResponseMessage;
+import com.Cinetime.repo.HallRepository;
+import com.Cinetime.repo.MovieRepository;
+import com.Cinetime.repo.PosterImageRepository;
+import com.Cinetime.repo.ShowtimeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 import java.time.LocalDate;
-import java.util.Optional;
+import java.util.List;
+
 
 @Service
+@RequiredArgsConstructor
 public class MovieService {
 
-    @Autowired
-    private MovieRepository movieRepository;
+    private final MovieRepository movieRepository;
+    private final PageableHelper pageableHelper;
+    private final MovieMapper movieMapper;
+    private final HallRepository hallRepository;
+    private final ShowtimeRepository showtimeRepository;
+    private final MovieHelper movieHelper;
+    private final PosterImageRepository posterImageRepository;
 
-    public Movie updateMovie(Long id, Movie movieDetails) {
-        Optional<Movie> movieOptional = movieRepository.findById(id);
-        if (!movieOptional.isPresent()) {
+
+    //M12
+    public MovieResponse updateMovie(Long id, MovieRequest movieRequest) {
+        Movie movie = movieRepository.findById(id).orElse(null);
+        if (movie == null) {
             return null;
         }
-
-        Movie movie = movieOptional.get();
-        movie.setTitle(movieDetails.getTitle());
-        movie.setSlug(movieDetails.getSlug());
-        movie.setSummary(movieDetails.getSummary());
-        movie.setReleaseDate(movieDetails.getReleaseDate());
-        movie.setDuration(movieDetails.getDuration());
-        movie.setRating(movieDetails.getRating());
-        movie.setDirector(movieDetails.getDirector());
-        movie.setCast(movieDetails.getCast());
-        movie.setFormats(movieDetails.getFormats());
-        movie.setGenre(movieDetails.getGenre());
-        movie.setPoster(movieDetails.getPoster());
-        movie.setStatus(movieDetails.getStatus());
-        movie.setUpdatedAt(LocalDate.now());
-
-        return movieRepository.save(movie);
+        movie = movieMapper.mapMovieRequestToMovie(movieRequest);
+        movie.setId(id);
+        movieRepository.save(movie);
+        return movieMapper.mapMovieToMovieResponse(movie);
     }
 
+    //M13
     public boolean deleteMovie(Long id) {
-        Optional<Movie> movieOptional = movieRepository.findById(id);
-        if (!movieOptional.isPresent()) {
+        if (!movieRepository.existsById(id)) {
             return false;
         }
-
-        movieRepository.delete(movieOptional.get());
-        return true;  // Movie deleted
+        movieRepository.deleteById(id);
+        return true;
     }
+
+    public ResponseEntity<List<Movie>> getMovieByHall(int page, int size, String sort, String type, String hall) {
+        Pageable pageable = pageableHelper.pageableSort(page, size, sort, type);
+
+        List<Movie> movies = movieRepository.findByHalls_NameIgnoreCase(hall,pageable);
+
+        if (movies.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.ok(movies);
+        }
+    }
+
+    public ResponseEntity<List<Movie>> getInTheatersMovies(int page, int size, String sort, String type) {
+
+        Pageable pageable = pageableHelper.pageableSort(page, size, sort, type);
+
+        List<Movie> movies = movieRepository.findByStatus(MovieStatus.IN_THEATERS, pageable);
+
+        if (movies.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.ok(movies);
+        }
+    }
+
+    public ResponseEntity<List<Movie>> getComingSoonMovies(int page, int size, String sort, String type) {
+        Pageable pageable = pageableHelper.pageableSort(page, size, sort, type);
+
+        List<Movie> movies = movieRepository.findByStatus(MovieStatus.COMING_SOON, pageable);
+
+        if (movies.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.ok(movies);
+        }
+    }
+
+
+    public ResponseMessage<Movie> createMovie(MovieRequest movieRequest) {
+        // Validate input
+        movieHelper.validateMovieRequest(movieRequest);
+
+        Movie newMovie = movieMapper.mapMovieRequestToMovie(movieRequest);
+
+        // Handle Hall relationship
+/*        Hall hallToBeSet = hallRepository.findById(movieRequest.getHallId())
+                .orElseThrow(() -> new ResourceNotFoundException(ErrorMessages.HALL_NOT_FOUND));
+        newMovie.getHalls().add(hallToBeSet);
+        hallToBeSet.getMovies().add(newMovie); // Update both sides of the relationship
+
+        // Handle Showtime relationship
+        Showtime showtimeToBeSet = showtimeRepository.findById(movieRequest.getShowtimeId())
+                .orElseThrow(() -> new ResourceNotFoundException(ErrorMessages.SHOWTIME_NOT_FOUND));
+        newMovie.getShowtimes().add(showtimeToBeSet);
+        showtimeToBeSet.setMovie(newMovie); // Update both sides of the relationship*/
+
+        // Handle poster image
+        if (movieRequest.getPosterImage() != null && !movieRequest.getPosterImage().isEmpty()) {
+            try {
+                PosterImage posterImage = new PosterImage();
+                posterImage.setName(movieRequest.getPosterImage().getOriginalFilename());
+                posterImage.setType(movieRequest.getPosterImage().getContentType());
+                posterImage.setData(movieRequest.getPosterImage().getBytes());
+                PosterImage posterImageSaved = posterImageRepository.save(posterImage);
+
+                newMovie.setPoster(posterImageSaved);
+            } catch (IOException e) {
+                return ResponseMessage.<Movie>builder()
+                        .message("Failed to process image: " + e.getMessage())
+                        .httpStatus(HttpStatus.BAD_REQUEST)
+                        .build();
+            }
+
+
+        }
+
+        // Save the movie
+        newMovie.setCreatedAt(LocalDate.now());
+        newMovie.setUpdatedAt(LocalDate.now());
+        Movie savedMovie = movieRepository.save(newMovie);
+
+        return ResponseMessage.<Movie>builder()
+                .message(SuccessMessages.MOVIE_CREATE)
+                .httpStatus(HttpStatus.CREATED)
+                .object(savedMovie)
+                .build();
+    }
+
+
 }
+/*
+    // Update a movie
+    public MovieResponse updateMovie(Long id, MovieRequest movieRequest) {
+        Movie movie = movieRepository.findByid(id).orElse(null);
+        if (movie == null) {
+            return null;
+        }
+        movie = movieMapper.toEntity(movieRequest);
+        movie.setId(id);
+        movieRepository.save(movie);
+        return movieMapper.toResponse(movie);
+    }
+
+    // Delete a movie
+    public boolean deleteMovie(Long id) {
+        if (!movieRepository.existsById(id)) {
+            return false;
+        }
+        movieRepository.deleteById(id);
+        return true;
+    }
+
+ */
+
 
 
 
--- src/main/java/com/Cinetime/controller/MovieController.java
+++ src/main/java/com/Cinetime/controller/MovieController.java
@@ -1,38 +1,80 @@
-package com.Cinetime.Cotroller;
+package com.Cinetime.controller;
 
-import com.Cinetime.Service.MovieService;
 import com.Cinetime.entity.Movie;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.Cinetime.payload.dto.MovieRequest;
+import com.Cinetime.payload.response.MovieResponse;
+import com.Cinetime.payload.response.ResponseMessage;
+import com.Cinetime.service.MovieService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/movies")
-
 public class MovieController {
 
-    @Autowired
     private MovieService movieService;
 
-    @PutMapping("/id")
-    public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody Movie movieDetails) {
-        Movie updatedMovie = movieService.updateMovie(id, movieDetails);
-
+    //M12
+    @PutMapping("/{movieId}")
+    public ResponseEntity<MovieResponse> updateMovie(@PathVariable Long movieId, @RequestBody MovieRequest movieRequest) {
+        MovieResponse updatedMovie = movieService.updateMovie(movieId, movieRequest);
         if (updatedMovie == null) {
             return ResponseEntity.notFound().build();
         }
-
         return ResponseEntity.ok(updatedMovie);
     }
 
-    @DeleteMapping("/id")
-    public ResponseEntity<Void> deleteMovie(@PathVariable Long id) {
-        boolean deleted = movieService.deleteMovie(id);
-
+    //M13
+    @DeleteMapping("/{movieId}")
+    public ResponseEntity<Void> deleteMovie(@PathVariable Long movieId) {
+        boolean deleted = movieService.deleteMovie(movieId);
         if (!deleted) {
             return ResponseEntity.notFound().build();
         }
-
         return ResponseEntity.noContent().build();
+    }
+
+    //M03
+    @GetMapping("/{hall}")
+    public ResponseEntity<List<Movie>>getMovieByHall(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "releaseDate") String sort, @RequestParam(defaultValue = "asc") String type,
+            @PathVariable String hall){
+
+        return movieService.getMovieByHall(page,size,sort,type,hall);
+    }
+
+    //M04
+    @GetMapping("/in-theaters")
+    public ResponseEntity<List<Movie>> getInTheatersMovies(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "releaseDate") String sort,
+            @RequestParam(defaultValue = "asc") String type) {
+
+        return movieService.getInTheatersMovies(page, size, sort, type);
+    }
+
+    //M05
+    @GetMapping("/coming-soon")
+    public ResponseEntity<List<Movie>> getComingSoonMovies(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "releaseDate") String sort,
+            @RequestParam(defaultValue = "asc") String type) {
+
+        return movieService.getComingSoonMovies(page, size, sort, type);
+    }
+
+
+    //M11
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')") // Explicitly allow all access to this method
+    public ResponseMessage<Movie> createMovie(@ModelAttribute MovieRequest movieRequest) {
+        return movieService.createMovie(movieRequest);
     }
 }
